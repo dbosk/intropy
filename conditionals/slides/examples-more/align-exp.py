2a7546fb4f68b76056e5b8b58237bf5df2c5f3d1
Daniel Bosk <dbosk@kth.se>
2020-09-04 15:34:52 +0200

    Adds comment to align-exp

--- conditionals/slides/examples-more/align-exp.py
+++ conditionals/slides/examples-more/align-exp.py
@@ -1,3 +1,9 @@
 n = "10"
+"""
+4.4f som format specification: den första fyran anger den *totala* bredden, 
+  inklusive decimalerna. Därav hade den ingen effekt i vårt exempel, då vi hade 
+  fyra decimaler och lite till. Om vi hade satt den till 10, skulle vi ha sett 
+  effekt.
+"""
 print(f"n = {float(n):4.4f}")
 
