931d739170a06a720d2a2a3179cb650e0b5a1e2c
Daniel Bosk <dbosk@kth.se>
2020-09-10 14:16:46 +0200

    Adds multiplication tables examples

---
+++ containers/slides/examples/multtable.py
@@ -0,0 +1,25 @@
+"""Print the multiplication table on screen."""
+
+# the number of spaces between columns
+SPACE = 1
+
+# print the top bar
+print("* |", end="")
+for y in range(1, 10):
+  print(f"{y:{SPACE+2}d}", end="")
+
+# print a separating line
+print("\n--+"+"-"*(10*(SPACE+2)-3))
+
+# everything below the line
+for x in range(1, 10):
+  # print the x column with the vertical bar
+  print(f"{x:1d} |", end="")
+  
+  # print the multiplications with proper spacing
+  for y in range(1, 10):
+    print(f"{x*y:{SPACE+2}d}", end="")
+
+  # break to a new line
+  print("")
+
