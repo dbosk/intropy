3eddaa773749047adc4588663af9b4d8d56afe30
Daniel Bosk <dbosk@kth.se>
2020-09-18 13:52:39 +0200

    Adds error handling for factorial example

--- functions/slides-more/examples/factorial.py
+++ functions/slides-more/examples/factorial.py
@@ -7,13 +7,17 @@ def factorial(num):
     if num == 1:
         return 1
     elif num < 1:
-        raise ArithmeticError("Must be positive numbers")
+        raise Exception("Must be positive numbers")
     return num*factorial(num-1)
 
 def main():
     """test the functions in this module"""
-    num = input_type(int, "Enter a number: ")
-    print(f"{num}! = {factorial(num)}")
+    num = input_type.input_type(int, "Enter a number: ")
+    try:
+        print(f"{num}! = {factorial(num)}")
+    except Exception as err:
+        print(err)
+        return
 
 if __name__ == "__main__":
     main()
