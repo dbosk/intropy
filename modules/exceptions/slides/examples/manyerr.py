9ce3d8c8b0bfb7f235fff6472239e237108bccad
Daniel Bosk <dbosk@kth.se>
2021-09-20 20:48:35 +0200

    Makes exceptions slides compile, change listing to minted

--- modules/exceptions/slides/examples/manyerr.py
+++ modules/exceptions/slides/examples/manyerr.py
@@ -3,13 +3,14 @@ while True:
         x = int(input("Nominator x = "))
         y = int(input("Demoninator y = "))
         print(f"{x} / {y} = {x/y}")
+        break
+
     except ZeroDivisionError:
         print("Sorry, the denominator must be non-zero.")
-        continue
+
     except ValueError:
         print("Sorry, you must enter numbers.")
-        continue
+
     except Exception as err:
         print(f"Sorry, an unexpected error occured: {err}")
-    break
 
