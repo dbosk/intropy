1c39c674475f1eea2642affdd7e3de0a07cb2a35
Daniel Bosk <dbosk@kth.se>
2021-10-05 12:57:16 +0200

    Improves files slides and examples

--- modules/files/slides/examples/xy.py
+++ modules/files/slides/examples/xy.py
@@ -3,7 +3,7 @@
 def main_write():
     """ Writes x, f(x) pairs to a file """
     x_list = range(100)
-    y_list = apply_f(x_list)
+    y_list = map(f, x_list)
     xy_pairs = zip(x_list, y_list)
     with open("xy.txt", "w") as file:
         for x_val, y_val in xy_pairs:
@@ -18,21 +18,15 @@ def main_read():
     for line in lines:
         line = line.strip()
         x_val, y_val = line.split(" -> ")
-        if 2*int(x_val)+3 != int(y_val):
+        if f(int(x_val)) != int(y_val):
             print(f"{x_val} -/> {y_val}")
             any_bad = True
 
     if not any_bad:
         print("All values looked good!")
 
-def apply_f(xs):
-    """Takes a list of x-values, xs, returns the list 2x+3"""
-    ys = []
-
-    for x in xs:
-        ys.append(2*x+3)
-
-    return ys
+def f(x):
+    return 2 * x + 3
 
 if __name__ == "__main__":
     main_write()
