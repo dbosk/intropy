3a4239f27928191a0374f4ca5d8cfd4ca0e293a9
Daniel Bosk <dbosk@kth.se>
2021-10-05 21:21:50 +0200

    Improves files SCB example summary.py

--- modules/files/slides/examples/scb/summary.py
+++ modules/files/slides/examples/scb/summary.py
@@ -1,6 +1,34 @@
 """Summerar statistik"""
 
+import operator
 import scb
+
+#   lambda x: x[1]
+#
+# är en namnlös funktion som gör detsamma som
+#
+#   def plocka_ut_andra_elementet(x):
+#       return x[1]
+#
+# Vilket i sin tur är detsamma som
+#
+#   operator.itemgetter(1)
+
+def sortera_efter_antal(namn_antal_lista):
+    """Tar en dictionary namn_antal_lista och
+    returnerar en lista med (namn, antal)-tupler sorterade efter antal"""
+    return sorted(namn_antal_lista.items(),
+            key=lambda x: x[1], reverse=True)
+
+# lista = [1, 2, 3, 4, 5]
+# lista[0] == 1: plockar ut element 0.
+# lista[0:2] == [1, 2]: skapar en lista med alla elementen 0, 1 (inte inkl 2)
+
+def better_topp_10(namn_antal_lista):
+    """Tar dictionary namn_antal_list och returnerar lista med de 10 vanligast 
+    förekommande namnen"""
+    sorterad = sortera_efter_antal(namn_antal_lista)
+    return sorterad[0:10]
 
 def max_antal(namn_antal_lista):
     """Returnerar det namn och antal som har störst antal"""
@@ -16,13 +44,23 @@ def max_antal(namn_antal_lista):
 
 def topp_10(namn_antal_lista):
     """Tar dictionary namn_antal_list och returnerar lista med de 10 vanligast förekommande namnen"""
+    # vi skapar en kopia, annars förstör vi originallistan
+    kopia = namn_antal_lista.copy()
     topplista = []
     for topp in range(10):
-        namn, antal = max_antal(namn_antal_lista)
+        namn, antal = max_antal(kopia)
         topplista.append((namn, antal))
-        namn_antal_lista.pop(namn)
+        kopia.pop(namn)
 
     return topplista
+
+def skriv_ut_topplista(namntyp, lista):
+    """Skriver ut en topp N-lista för namn av typen namntyp (str) och listan 
+    lista med topplistan, längden på lista används för N"""
+
+    print(f"Topp {len(lista)} kvinnliga tilltalsnamn:")
+    for nummer, (namn, antal) in enumerate(lista):
+        print(f"{nummer+1}: {namn}, {antal} personer")
 
 def main():
     """Huvudprogram"""
@@ -32,8 +70,10 @@ def main():
     förnamn_kvinnor = scb.läs_in_namn_antal("förnamn-kvinnor-2020.csv")
     tilltalsnamn_män = scb.läs_in_namn_antal("tilltalsnamn-män-2020.csv")
 
-    print("Topp 10 kvinnliga tilltalsnamn:")
-    print(topp_10(tilltalsnamn_kvinnor))
+    skriv_ut_topplista("kvinnliga tilltalsnamn",
+        topp_10(tilltalsnamn_kvinnor))
+    skriv_ut_topplista("Topp 10 kvinnliga tilltalsnamn:",
+        better_topp_10(tilltalsnamn_kvinnor))
 
 if __name__ == "__main__":
     main()
