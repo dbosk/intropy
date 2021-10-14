fd31b262c558ca2345947a0a02cdfc45086a5dc6
Daniel Bosk <dbosk@kth.se>
2021-10-14 10:55:36 +0200

    Adds code for Skatteverket for classes tutorial

---
+++ modules/classes/tutorial/address.py
@@ -0,0 +1,53 @@
+"""Module med en klass för att hantera adresser"""
+
+class Adress:
+    """Klass för att hantera adresser"""
+    def __init__(self, gata, nummer, postnummer, postort,
+            land="Sverige"):
+        """Returnerar ett adressobjekt"""
+        self.__gata = gata
+        self.__nummer = nummer
+        self.__postnummer = postnummer
+        self.__postort = postort
+        self.__land = land
+
+    def __str__(self):
+        """Returnerar sträng för utskrift"""
+        adress = f"{self.__gata} {self.__nummer}, " + \
+                 f"{self.__postnummer} {self.__postort}"
+
+        if self.__land != "Sverige":
+            adress += f", {self.__land}"
+
+        return adress
+
+    def gatan(self):
+        """Returnerar gatan i adressen"""
+        return self.__gata
+
+    def orten(self):
+        """Returnerar numret i adressen"""
+        return self.__postort
+
+    def __eq__(self, other):
+        """Jämför två adresser, returnerar True om de är identiska"""
+        return self.__gata == other.__gata and \
+               self.__nummer == other.__nummer and \
+               self.__postnummer == other.__postnummer and \
+               self.__postort == other.__postort and \
+               self.__land == other.__land
+
+def main():
+    """Testprogram"""
+    daniels_kontor = Adress("Lindstedtsvägen", "5", "11428", "Stockholm")
+    print(f"Daniel har sitt arbtesrum på adressen {daniels_kontor}.")
+    print(f"Gatan är {daniels_kontor.gatan()}.")
+
+    olles_kontor = Adress("Lindstedtsvägen", "3", "11428", "Stockholm")
+    if daniels_kontor == olles_kontor:
+        print("Daniel och Olle har arbetsrum på samma adress.")
+    else:
+        print("Daniel och Olle har inte arbetsrum på samma adress.")
+
+if __name__ == "__main__":
+    main()
---
+++ modules/classes/tutorial/skatteverket.py
@@ -0,0 +1,66 @@
+""" Reperesenting Skatteverket's pupulation register """
+
+import person
+from address import Adress as Address
+
+# vi kan ha "samma namn" på dem, fast egentligen är det inte samma namn:
+# den ena heter Person och den andra person.Person.
+class Person(person.Person):
+    """ Represents a real/physical person """
+    def __init__(self, person_id, full_name, parents, address):
+        names = full_name.split()
+        super().__init__(" ".join(names[:-1]), names[-1], person_id, address)
+        self.__parents = parents
+        self.__address = address
+        self.__old_addresses = []
+
+    def __str__(self):
+        """Returns a string representation of the person"""
+        return f"{self.get_name()} ({self.get_personnummer()})"
+
+    def get_parents(self):
+        """ Returns the parents """
+        return self.__parents.copy()
+
+    def change_address(self, new_address):
+        """ Changes the address (registers a move) """
+        self.__old_addresses.append(self.__address)
+        self.__address = new_address
+
+
+def main():
+    """ Skatteverket test program """
+    folkbokföring = {}
+
+    alice = Person("1950-01-01-1234", "Alice Adamsdotter", None,
+                   Address("Vägen", "1", "12345", "Orten"))
+
+    folkbokföring["1950-01-01-1234"] = alice
+
+    bob = Person("1950-01-01-1244", "Bob Evasson", None,
+                 Address("Vägen", "1", "12345", "Orten"))
+
+    folkbokföring["1950-01-01-1244"] = bob
+
+    cecilia = Person("1970-01-01-1234", "Cecilia Adamsdotter-Evasson",
+                     [alice, bob],
+                     Address("Vägen", "1", "12345", "Orten"))
+
+    folkbokföring["1970-01-01-1234"] = cecilia
+
+    print(f"{cecilia} bor på {cecilia.get_address()}")
+
+    cecilia.change_address(Address("Vägen", "2", "12345", "Orten"))
+    print(f"{cecilia} bor på {cecilia.get_address()}")
+
+    parents = cecilia.get_parents()
+    print(f"{cecilia}s föräldrar heter {parents[0]} och {parents[1]}.")
+
+    print(f"{alice} och {bob} bor fortfarande på {alice.get_address()}")
+
+    print("Folkbokföringen")
+    for person in sorted(folkbokföring.values()):
+        print(person)
+
+if __name__ == "__main__":
+    main()
