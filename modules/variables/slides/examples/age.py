59315dcc36354bf1fc751c77bee09abd5e7a0fd5
Daniel Bosk <dbosk@kth.se>
2022-09-01 14:52:12 +0200

    Improves variables slides
    Adds one big example program as "the whole", then discusses it's parts
    (through variation).

--- modules/variables/slides/examples/age.py
+++ modules/variables/slides/examples/age.py
@@ -13,14 +13,16 @@ birthyear = 1985
 
 print(f"Hej, {name}")
 
+# beräkna ålder
 this_year = 2022
 age = this_year - birthyear
 
-print(f"Du är {this_year-birthyear} gammal.")
+print(f"Du är {age} gammal.")
 
 print("Nu ska du får bli 25 år igen.")
 
-# beräkna ny ålder
+# beräkna nytt födelseår
 birthyear = this_year - 25
 
-print(f"Du är {this_year-birthyear} gammal.")
+print(f"Du är {this_year-birthyear} år gammal, "
+      f"det är {age-(this_year-birthyear)} år yngre.")
