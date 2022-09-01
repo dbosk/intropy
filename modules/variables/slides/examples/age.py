30154abe24b4369bdb27c59dc3c7bf7eb89944bc
Daniel Bosk <dbosk@kth.se>
2022-09-01 13:22:02 +0200

    Removes redundant examples, adds age example

---
+++ modules/variables/slides/examples/age.py
@@ -0,0 +1,26 @@
+"""
+Ett program om ålder. Illustrerar:
+
+    - kommentarer,
+    - utskrifter,
+    - variabler,
+    - datatyper
+    - variabelnamn och konstanter
+"""
+
+name = "Daniel"
+birthyear = 1985
+
+print(f"Hej, {name}")
+
+this_year = 2022
+age = this_year - birthyear
+
+print(f"Du är {this_year-birthyear} gammal.")
+
+print("Nu ska du får bli 25 år igen.")
+
+# beräkna ny ålder
+birthyear = this_year - 25
+
+print(f"Du är {this_year-birthyear} gammal.")
