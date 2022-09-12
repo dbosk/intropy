382862e69c9842d602f54cb228ff121d4c9beeee
Daniel Bosk <dbosk@kth.se>
2022-09-12 09:17:11 +0200

    Moves conditionals examples between slides

--- modules/conditionals/slides-conditionals/examples/birthyear.py
+++ modules/conditionals/slides-conditionals/examples/birthyear.py
@@ -1,10 +1,10 @@
 """Program som tar ålder som inmatning"""
 
-år = int(input("När föddes du?"))
+year = int(input("När föddes du? "))
 
-if år < 1995:
-    print("Du är gammal och grå!")
-elif år < 2000:
+if year < 2000:
     print("Du är i hyfsad form ändå.")
+elif year < 1990:
+    print("Du är gammal och grå!")
 else:
     print("Du är ung och fräsch! Än så länge!")
