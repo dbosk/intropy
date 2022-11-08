788946b5de278f7e955f234cbfcda3eb55ffcb04
Daniel Bosk <dbosk@kth.se>
2022-11-08 08:39:46 +0100

    Improves amanuens.py: can move start date

--- adm/tid/amanuens.py
+++ adm/tid/amanuens.py
@@ -1,3 +1,4 @@
+import arrow
 import csv
 import nytid.schedules as sched
 import nytid.schedules.utils as utils
@@ -14,12 +15,32 @@ COURSES = {
 def to_hours(td):
     return td.total_seconds()/60/60
 
+def shift_dates_forward(start_date, end_date):
+    """Takes dates and shifts them so that start_date is tomorrow."""
+    now = arrow.now()
+    today = arrow.Arrow(now.year, now.month, now.day,
+                        hour=start_date.hour, minute=start_date.minute,
+                        second=start_date.second)
+
+    if start_date > today:
+        return start_date, end_date
+
+    diff = (today-start_date).days
+    return start_date.shift(days=diff+1), end_date.shift(days=diff+1)
+
+
 if len(sys.argv) < 2:
     print(f"{sys.argv[0]}: requires argument 'username'",
           file=sys.stderr)
+    print(f"{sys.argv[0]} <username> [<start date>]")
     sys.exit(1)
 else:
     user = sys.argv[1]
+
+if len(sys.argv) > 2:
+    date = arrow.get(sys.argv[2])
+else:
+    date = None
 
 prgi22 = utils.read_signup_sheet_from_url(
         utils.google_sheet_to_csv_url(
@@ -29,11 +50,17 @@ prgm22 = utils.read_signup_sheet_from_url(
         utils.google_sheet_to_csv_url(
             "https://docs.google.com/spreadsheets/d/18-afYjlI--e8hDwzSp9VcHjatc7SPvBFW0fH9JRrZ-4/edit#gid=1755336853"))
 
-amanuensis = utils.compute_amanuensis_data(prgi22 + prgm22)
+amanuensis = utils.compute_amanuensis_data(prgi22 + prgm22,
+                                           begin_date=date)
 data = amanuensis[user]
 
-print(f"{user}: {data[2]:.2f} h, {100*utils.compute_percentage(*data):.1f}%: "
-      f"{data[0].format('YYYY-MM-DD')}--{data[1].format('YYYY-MM-DD')}")
+#start, end = shift_dates_forward(data[0], data[1])
+start = data[0]
+end = data[1]
+
+print(f"{user}: {data[2]:.2f} h, "
+      f"{round(100*utils.compute_percentage(*data))}%: "
+      f"{start.format('YYYY-MM-DD')}--{end.format('YYYY-MM-DD')}")
 
 prgx = utils.filter_events_by_TA(user, sorted(prgi22 + prgm22,
          key=operator.itemgetter(utils.SIGNUP_SHEET_HEADER.index("Start"))))
