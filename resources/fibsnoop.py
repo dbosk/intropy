3362f6ce385802d7a50feee710391d33adcb32f6
Daniel Bosk <dbosk@kth.se>
2022-12-06 23:18:56 +0100

    Adds example for using snoop for teaching

---
+++ resources/fibsnoop.py
@@ -0,0 +1,17 @@
+"""Program som skriver ut Fibonacci-serien"""
+
+import snoop
+
+snoop.install(columns="")
+
+def fib(n):
+    """Returnerar n:te talet i Fibonacci-serien"""
+    if n <= 0:
+        return 0
+    if n == 1:
+        return 1
+
+    return fib(n-1) + fib(n-2)
+
+with snoop(depth=10):
+    print(f"fib(5) = {fib(5)}")
