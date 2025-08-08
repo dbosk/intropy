faa00624857e9e0914b087b9ae75e00b948f388f
Daniel Bosk <dbosk@kth.se>
2025-08-08 09:13:17 +0200

    Stylistic changes to prgx.sh

--- adm/prgx.sh
+++ adm/prgx.sh
@@ -3,9 +3,9 @@
 courses="(prgi[2-9][0-9]|prgm2[1-9]|prgm[3-9][0-9])"
 components="(LAB[13]|MAT1|KAL1)"
 
-canvaslms results -c "$courses" -A "$components" | \
-  sed -E "s/ ?[HV]T[0-9]{2}( \(.*\))?//" | \
-  ladok report -fv
+canvaslms results -c "$courses" -A "$components" \
+| sed -E "s/ ?[HV]T[0-9]{2}( \(.*\))?//" \
+| ladok report -fv
 
 oldIFS="$IFS"
 IFS=$'\n'
@@ -18,6 +18,6 @@ IFS="${oldIFS}"
 datorprov_courses="(prg[mi]2[2-9]|prg[mi][3-9][0-9])"
 
 canvaslms results -c "$datorprov_courses" -A "LAB2" \
-  -S canvaslms.grades.maxgradesurvey | \
-    sed -E "s/ ?[HV]T[0-9]{2}( \(.*\))?//" | \
-    ladok report -fv
+  -S canvaslms.grades.maxgradesurvey \
+| sed -E "s/ ?[HV]T[0-9]{2}( \(.*\))?//" \
+| ladok report -fv
